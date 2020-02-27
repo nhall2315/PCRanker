8f51f4b736bb713707c36545ab0052494844e6ff
Noah Hall <n.hall@kunzleigh.com>
2020-02-27 10:41:00 -0500

    Converted app from css to sass

--- PCRanker/ClientApp/src/app/manage-builds/manage-builds.component.ts
+++ PCRanker/ClientApp/src/app/manage-builds/manage-builds.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manage-builds',
   templateUrl: './manage-builds.component.html',
-  styleUrls: ['./manage-builds.component.css']
+  styleUrls: ['./manage-builds.component.sass']
 })
 export class ManageBuildsComponent implements OnInit {
 
--- PCRanker/ClientApp/src/app/part-browser/part-browser.component.ts
+++ PCRanker/ClientApp/src/app/part-browser/part-browser.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-part-browser',
   templateUrl: './part-browser.component.html',
-  styleUrls: ['./part-browser.component.css']
+  styleUrls: ['./part-browser.component.sass']
 })
 export class PartBrowserComponent implements OnInit {
 
